0f17eb4cddce23594ef8ebdecc4c54401f46bb48
JonVanTael <hello@cyclinghub.be>
2025-04-25 09:22:25 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ const app = express();
 
 const CLIENT_ID = '156548';
 const CLIENT_SECRET = 'a2e6928a8fe8a9461dafb60f799d30da5a0d20e8';
-let accessToken = 'c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9d';
-let refreshToken = 'd2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9d0e';
-let tokenExpiresAt = 1745852400 * 1000;
+let accessToken = 'f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a';
+let refreshToken = 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b';
+let tokenExpiresAt = 1745856000 * 1000;
 
 app.use(express.json());
 app.use((req, res, next) => {
